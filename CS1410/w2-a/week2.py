6fa1f10055377edb465e133ac7d79055a6c7d9c0
connor <conrokey73@gmail.com>
2026-09-03 09:14:37 -0600

    making it so week2.py runs

--- CS1410/w2-a/week2.py
+++ CS1410/w2-a/week2.py
@@ -1,8 +1,16 @@
-a = 25
-x = 10
-b = 5
-y = 2
-c = 9
+money = float(input("Enter the amount of merchandise: "))
 
-z = (a * x) + (b * y) + c
-print(z)
+if money >= 500:
+    discount = .4
+    print()
+elif 500 > money >= 200:
+    discount = .2
+elif money < 100:
+    discount = .1
+else:
+    discount = 0
+
+
+total = money * (1 - discount)
+print(f"Discount applied: {discount * 100:.0f}%")
+print(f"Final price: ${total:.2f}")
