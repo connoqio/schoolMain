a76a7b100dc92253db0bdbe99349eeb4de5bb7b1
connor <conrokey73@gmail.com>
2026-09-03 11:16:18 -0600

    final product of customerDiscount.py

--- CS1410/w2-a/CustomerDiscount.py
+++ CS1410/w2-a/CustomerDiscount.py
@@ -1,5 +1,13 @@
-money = float(input("Enter the amount of merchandise: "))
+"""
+Connor made this program after he popped a tired going to school
+it is currently 9:23am on 9/3/2026
+this program took about 15 minutes to figure out and write
+"""
 
+money = float(input("Enter the amount of merchandise: "))  #this gets the input from the user and converts it to a float
+print(" ")  #this prints a blank line to make the output look nicer
+
+# this if elif and else statement checks how much money the user entered
 if money >= 500:
     discount = .4
     print()
@@ -11,6 +19,7 @@ else:
     discount = 0
 
 
-total = money * (1 - discount)
-print(f"Discount applied: {discount * 100:.0f}%")
+total = money * (1 - discount) 
+print(f"You get  {discount * 100:.0f}% off!" "\n")
+
 print(f"Final price: ${total:.2f}")
