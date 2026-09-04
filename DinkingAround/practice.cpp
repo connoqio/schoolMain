0e5fc986b038bab00988e6c2efd1e8688b2ffd43
connor <conrokey73@gmail.com>
2026-09-03 20:12:09 -0600

    more temp changes

--- DinkingAround/practice.cpp
+++ DinkingAround/practice.cpp
@@ -4,14 +4,28 @@ using namespace std;
 
 
 
-int main(){
-    double tempf;
-    cout << "what temperature do you want to convert to celsius?" << endl;
-    cin >> tempf;
-    double tempc = (tempf -32) * 5/9;
+// int main(){
+//     double tempf;
+//     cout << "what temperature do you want to convert to celsius?" << endl;
+//     cin >> tempf;
+//     double tempc = (tempf -32) * 5/9;
 
-    cout << tempc;
+//     cout << tempc;
+
+//     return 0;
+
+// };
+
+
+int main(){
+    double farenheit = 0;
+    double celcius = 0;
+
+    cout << "what temperature do you want to convert to celsius?" << endl;
+    cin >> farenheit;
+
+    celcius = 5 / 9 * (farenheit - 32);
+    cout << "the celsius temperature is: " << celcius << endl;
 
     return 0;
-
-};
+}
