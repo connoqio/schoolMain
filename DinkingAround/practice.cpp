e87fa26b253fc6898d9476a62624f5f3da35019a
connor <conrokey73@gmail.com>
2026-09-03 20:43:22 -0600

    now i can debug in the terminal

--- DinkingAround/practice.cpp
+++ DinkingAround/practice.cpp
@@ -1,31 +1,19 @@
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 using namespace std;
 
 
 
-// int main(){
-//     double tempf;
-//     cout << "what temperature do you want to convert to celsius?" << endl;
-//     cin >> tempf;
-//     double tempc = (tempf -32) * 5/9;
-
-//     cout << tempc;
-
-//     return 0;
-
-// };
-
-
 int main(){
-    double x1, x2, y1, y2 = 0;
-    cout << "enter the coordinates of the first point (x1 y1): ";
-    cin >> x1 >> y1;
+    double x1, x2, y1, y2, z1, z2 = 0;
+    cout << "enter the coordinates of the first point (x1 y1 z1): ";
+    cin >> x1 >> y1 >> z1;
 
-    cout << "enter the coordinates of the second point (x2 y2): ";
-    cin >> x2 >> y2;
+    cout << "enter the coordinates of the second point (x2 y2 z2): ";
+    cin >> x2 >> y2 >> z2;
 
-    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2) + pow(z2 - z1, 2));
     cout << "the distance between the two points is: " << distance << endl;
 
     return 0;
