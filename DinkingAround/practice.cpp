af8e6c42f720c4c839cf93b4555b96aba07d4d23
connor <conrokey73@gmail.com>
2026-09-03 20:28:11 -0600

    created a launch.json file

--- DinkingAround/practice.cpp
+++ DinkingAround/practice.cpp
@@ -18,14 +18,15 @@ using namespace std;
 
 
 int main(){
-    double farenheit = 0;
-    double celcius = 0;
+    double x1, x2, y1, y2 = 0;
+    cout << "enter the coordinates of the first point (x1 y1): ";
+    cin >> x1 >> y1;
 
-    cout << "what temperature do you want to convert to celsius?" << endl;
-    cin >> farenheit;
+    cout << "enter the coordinates of the second point (x2 y2): ";
+    cin >> x2 >> y2;
 
-    celcius = 5 / 9 * (farenheit - 32);
-    cout << "the celsius temperature is: " << celcius << endl;
+    double distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    cout << "the distance between the two points is: " << distance << endl;
 
     return 0;
 }
