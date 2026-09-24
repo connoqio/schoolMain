33a210f78d25d4c8dd90e7eed69f37f9dcd9c68b
connor <conrokey73@gmail.com>
2026-09-24 11:18:07 -0600

    wrote more

--- WEB1430/week5/SecretAgentDashboard/script.js
+++ WEB1430/week5/SecretAgentDashboard/script.js
@@ -1,5 +1,9 @@
 function initializeDashboard(){
     const container = document.getElementById('mission-container');
 
+    const heading = document.createElement('h1');
+    heading.textContent = 'Secret Agent Dashboard';
+    container.prepend(heading);
 }
 
+initializeDashboard();
